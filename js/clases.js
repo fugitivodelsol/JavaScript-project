a29c4abcd744b0698bb4fd6ec33330e3f900c22b
Lorenzo Taborda <lrtaborda@gmail.com>
2023-03-25 23:40:30 -0300

    Mejora de la organización del código
    Se mejoró la organización del codigo.

--- js/clases.js
+++ js/clases.js
@@ -1,46 +1,42 @@
-let clientesArray = []
-let localesArray = []
-let reservasArray = []
-
-
-
+let clientesArray = [];
+let localesArray = [];
+let reservasArray = [];
+//
 let proximoIdClientes = 1;
 class Clientes {
-    constructor (pNombre, pContraseña,pReservasHechas) {
-        this.id = proximoIdClientes;
-        this.nombreUsuario = pNombre;
-        this.contraseñaUsuario = pContraseña
-        this.reservasHechas = pReservasHechas
+  constructor(pNombre, pContraseña, pReservasHechas) {
+    this.id = proximoIdClientes;
+    this.nombreUsuario = pNombre;
+    this.contraseñaUsuario = pContraseña;
+    this.reservasHechas = pReservasHechas;
 
-        proximoIdClientes++
-    }
+    proximoIdClientes++;
+  }
 }
-
+//
 let proximoIdLocales = 1;
 class Locales {
-    constructor (pNombre, pContraseña, pCapacidad, pCapacidadTotal) {
-        this.id = proximoIdLocales
-        this.nombreLocal = pNombre;
-        this.contraseñaLocal = pContraseña
-        this.capacidad = pCapacidad
-        this.capacidadTotal = pCapacidadTotal
-        
-        proximoIdLocales++
-    }
+  constructor(pNombre, pContraseña, pCapacidad, pCapacidadTotal) {
+    this.id = proximoIdLocales;
+    this.nombreLocal = pNombre;
+    this.contraseñaLocal = pContraseña;
+    this.capacidad = pCapacidad;
+    this.capacidadTotal = pCapacidadTotal;
 
+    proximoIdLocales++;
+  }
 }
-
+//
 let proximoIdReserva = 1;
 class Reservas {
-    constructor (pNombre, pCantidad, pLocal, pEstado, pCalificacion){
-        this.idReserva = proximoIdReserva
-        this.nombreCliente = pNombre;
-        this.cantidadDePersonas = pCantidad
-        this.local = pLocal
-        this.estado = pEstado
-        this.calificacion=pCalificacion
-        
-        proximoIdReserva++
-    }
-    
+  constructor(pNombre, pCantidad, pLocal, pEstado, pCalificacion) {
+    this.idReserva = proximoIdReserva;
+    this.nombreCliente = pNombre;
+    this.cantidadDePersonas = pCantidad;
+    this.local = pLocal;
+    this.estado = pEstado;
+    this.calificacion = pCalificacion;
+
+    proximoIdReserva++;
+  }
 }
